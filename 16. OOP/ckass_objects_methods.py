c2c21371482db0dbb26d4d8f2cbe8bbad17701c7
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-08-18 02:11:37 +0530

    commit

---
+++ 16. OOP/ckass_objects_methods.py
@@ -0,0 +1,21 @@
+class Student:
+    # Methods
+    def setDetails(self):
+        self.idd = int(input("Enter ID = "))
+        self.name = input("Enter name = ")
+        self.age = int(input("Enter age = "))
+        self.gender = input("Enter gender = ")
+        self.address = "Surat"
+        # self.display()
+
+    def display(self):
+        print(f"ID = {self.idd}")
+        print(f"Name = {self.name}")
+        print(f"Age = {self.age}")
+        print(f"Gender = {self.gender}")
+        print(f"Address = {self.address}")
+
+
+s1 = Student()
+s1.setDetails()
+s1.display()
