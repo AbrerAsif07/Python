cbcbffe1b18f1a6c40af08d94d590f17dea852c8
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-08-21 16:25:27 +0530

    Complete banking system

--- 16. OOP/banking_system.py
+++ 16. OOP/banking_system.py
@@ -25,6 +25,15 @@ class Banking:
         self.branch = branch
         self.balance = 0
 
+    def getBalance(self) -> int:
+        return self.balance
+
+    def deposit(self, amount) -> int:
+        self.balance += amount
+
+    def withdraw(self, amt):
+        self.balance -= amt
+
     def display(self):
         print(f"acc_no = {self.acc_no}")
         print(f"Name = {self.name}")
@@ -38,6 +47,9 @@ accounts: List[Banking] = []
 while True:
     print("1) Add account details")
     print("2) Display all account details")
+    print("3) Check balance of an account")
+    print("4) Deposit balance")
+    print("5) Withdraw money")
     print("6) Exit")
 
     choice = int(input("Enter your choice: "))
@@ -48,4 +60,40 @@ while True:
         bra = input("ENter branch= ")
         x = Banking(acc, name, acc_type, bra)
         accounts.append(x)
-        print(accounts)
+
+    elif choice == 2:
+        for acc in accounts:
+            acc.display()
+
+    elif choice == 3:
+        bl_show = int(input("enter acc no whose balance u want to check = "))
+        for acc in accounts:
+            if acc.acc_no == bl_show:
+                print(f"Your balance is = {acc.getBalance()}")
+            else:
+                print("no such accounts")
+
+    elif choice == 4:
+        bl = int(input("enter acc no where you want to deposit = "))
+        b2 = int(input("Enter amount you wish to deposit = "))
+        for acc in accounts:
+            if acc.acc_no == bl:
+                acc.deposit(b2)
+                print("your money has been deposited successfully")
+            else:
+                print("no such accounts")
+
+    elif choice == 5:
+        bl = int(input("enter acc no where you want to withdraw = "))
+        b2 = int(input("Enter amount you wish to withdraw = "))
+        for acc in accounts:
+            if acc.acc_no == bl:
+                acc.withdraw(b2)
+                print("your money has been wuthdrawn successfully")
+            else:
+                print("no such accounts")
+
+    elif choice == 6:
+        break
+    else:
+        print("invalid input")
